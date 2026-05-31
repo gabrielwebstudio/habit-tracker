05e85c6230b9379c467c8fb213a415510d9cff29
Gabriel <garey91@gmail.com>
2026-05-31 12:52:27 +0200

    Button style variants

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Button from './Button'
 
 export default function HabitForm() {
@@ -7,7 +6,7 @@ export default function HabitForm() {
         <input className='flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500'
         placeholder='New habit...'
         />
-        <Button>Add Habit</Button>
+        <Button className='rounded-lg px-4 py-2 font-medium'>Add Habit</Button>
     </form>
   )
 }
